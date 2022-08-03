7f38b84f26b9ede2b38237458bea2809898ef836
rickie maweu <rickiemaweu@gmail.com>
2022-08-03 16:07:35 +0300

    sumiya

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="header">
+      issa,s 444 and sumiya,s 333
+    </span>
+  );
+};
+
+export default Header;
