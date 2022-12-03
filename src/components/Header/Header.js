1bc9cbb05c205a6855a71323b38c9cd6415c53e8
rickie maweu <rickie.maweu@student.moringaschool.com>
2022-12-03 17:52:14 +0300

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <span onClick={() => window.scroll(0, 0)} className="header">
-      issa,s 444 and sumeiya,s 333
+      Movie Core
     </span>
   );
 };
